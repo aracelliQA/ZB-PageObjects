3971657a579c63d8473689b5f7fd6679eb054581
aracelliQA <aracellieng@gmail.com>
2023-01-31 16:11:16 -0300

    commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,34 @@
 
 import { addMatchImageSnapshotCommnad} from 'cypress-image-snapshot/command'
 addMatchImageSnapshotCommnad()
+
+Cypress.Commands.add('isVisible', selector => {
+    cy.get(selector).should('be.visible')
+})
+
+Cypress.Commands.add('isHidden',  selector => {
+    cy.get(selector).should('not.exist')
+})
+
+Cypress.Commands.add('setResolution', size => {
+    if(Cypress._.isArray(size)){
+        cy.viewport(size[0], size[1])
+    } else {
+        cy.viewport(size)
+    }
+})
+
+Cypress.Commands.add('login', (username,password) => {
+    cy.get('#login_form').should('be.visible')
+    cy.get('#user_login').type(username)
+      cy.get('#user_password').type(password)
+      cy.get('#user_remember_me').click()
+      cy.contains('Sign in').click()
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.contains('username').click()
+    cy.get('#logout_link').click()
+    cy.url().should('include', 'index.html')
+
+})

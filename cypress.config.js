dd9a74049ae635a0a7d62aa98404b906c725dc17
aracelliQA <aracellieng@gmail.com>
2023-01-31 12:55:04 -0300

    project setup

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'uvuuff',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  watchForFileChanges: false,
+  chromeWebSecurity: false,
+  viewportWidth: 1000,
+  viewportHeight: 600,
+  waitForAnimations: true,
+  animationDistanceThreshold:20,
+  defaultCommandTimeout: 6000,
+  execTimeout: 60000,
+  pageLoadTimeout: 60000,
+  requestTimeout: 15000,
+  responseTimeout: 15000,
+  video: true,
+  failOnStatusCode: false,
+  
+});
